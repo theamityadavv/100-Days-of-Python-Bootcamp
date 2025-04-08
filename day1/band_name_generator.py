e6dcf0d68c6d7d36477101cd4605526a8e0d9e9e
Amit Yadav <133171339+theamityadavv@users.noreply.github.com>
2025-04-08 19:36:51 +0530

    Create band_name_generator.py

---
+++ day1/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generato\n")
+city= input("enter the name of the city in which you grew up\n")
+pet_name = input("Enter the name of your pet\n")
+band_name=city+pet_name
+print("Your band name is "+ city+ pet_name +"\n")
